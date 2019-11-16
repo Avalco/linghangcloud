ba8bb181fac11f87115916714c597876c67c1d39
Avalco <44090235+Avalco@users.noreply.github.com>
2019-11-16 18:48:39 +0800

    Update Task.java

--- app/src/main/java/com/linghangcloud/android/db/Task.java
+++ app/src/main/java/com/linghangcloud/android/db/Task.java
@@ -3,6 +3,7 @@ package com.linghangcloud.android.db;
 public class Task {
     private String headline; // 任务标题
     private  String group; // 组别
+      @SerializedName("details")
     private  String content; // 任务内容
     private String taskid; // 任务id
     private String createtime; // 创建时间
